558fe59f8b14ec76b800aa6908e3d8aca295c9f3
AUTHENTIC <leogwork@gmail.com>
2020-11-27 17:42:25 +0800

    Fix · Issue of HANDLE's value always return 0 .

--- case/extr_db_krnl.py
+++ case/extr_db_krnl.py
@@ -1,4 +1,5 @@
 from ctypes import *
+from ctypes.wintypes import *
 
 '''*****************************************************************************************************************
     被测数据库：      SQL Server 2017 /MySQL5.7
@@ -21,27 +22,14 @@ from ctypes import *
 *****************************************************************************************************************'''
 
 
-class POINT(Structure):
-    _fields_ = [("x", c_int),
-                ("y", c_int)]
-
-
-class Test(Structure):
-    pass
-Test._fields_ = [('x', c_int),
-                 ('y', c_char),
-                 ('next', POINTER(Test))]
-
-
 def mount_db_file():
-    dll = cdll.LoadLibrary(r'../x64/extr_db_krnl.dll')      # 载入按标准的cdecl调用协议导出的函数
-    db_name = c_wchar_p('../data/debug/student.ibd')
+    dll = windll.LoadLibrary(r'../x64/extr_db_krnl.dll')       # 载入按stdcall调用协议调用其中的函数
+    db_name = c_wchar_p(r'C:\\Users\\XLY_LR\\PycharmProjects\\dll_test\\data\\debug\\score.ibd')
     db_type = 0x0A01
-    err = None
-    dll.extr_db_fun_A.restype = c_long
-    db_fh = pointer(c_long(1))
-    print(db_fh)
-    db_fh = dll.extr_db_fun_A(db_name, db_type, id(err))    # id()函数用于获取对象的内存地址
+    err = c_ulong()
+    dll.extr_db_fun_A.argtypes = [c_wchar_p, DWORD, POINTER(DWORD)]
+    dll.extr_db_fun_A.restype = HANDLE
+    db_fh = dll.extr_db_fun_A(db_name, db_type, byref(err))    # id()函数用于获取对象的内存地址
     print(err)
     print('HANDLE =', db_fh)
     print(type(db_fh))
