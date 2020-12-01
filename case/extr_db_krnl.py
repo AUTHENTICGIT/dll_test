43215df1a5e33c79da0d1b12524661c887f5dc29
AUTHENTIC <leogwork@gmail.com>
2020-12-01 14:34:21 +0800

    Update · Combine mount and unmount

--- case/extr_db_krnl.py
+++ case/extr_db_krnl.py
@@ -19,37 +19,77 @@ from ctypes.wintypes import *
                     fSampleDb   # 样本数据库，默认情况可以不传
     返回：
                     数据库装载句柄
+    接口：
+                    # 装载数据库文件接口
+                    void extr_db_fun_B(HANDLE &hMntDB);
+    参数：
+                    hMntDB      # 数据库装载句柄
+    返回：
+                    无
 *****************************************************************************************************************'''
 
 
-def mount_db_file():
-    dll = windll.LoadLibrary(r'../x64/extr_db_krnl.dll')       # 载入按stdcall调用协议调用其中的函数
-    db_name = r'C:\\Users\\XLY_LR\\PycharmProjects\\dll_test\\data\\debug\\score.ibd'
-    db_type = 0x0A01
-    err = c_ulong()
-    dll.extr_db_fun_A.argtypes = [c_wchar_p, DWORD, POINTER(DWORD)]
-    dll.extr_db_fun_A.restype = HANDLE
-    db_fh = dll.extr_db_fun_A(db_name, db_type, byref(err))    # byref()函数用于获取对象的内存地址，db_fh是<class 'int'>
+class MountAndUnmount:
+    def __init__(self):
+        self.dll = windll.LoadLibrary(r'../x64/extr_db_krnl.dll')   # 载入按stdcall调用协议调用其中的函数
+        self.db_name = r'C:\Users\XLY_LR\PycharmProjects\dll_test\data\debug\score.ibd'
+        self.db_type = 0x0A01
+        self.err = DWORD(111)
+        self.db_fh = HANDLE()
 
-    # dll = cdll.LoadLibrary(r'..\x64\extr_db_krnl.dll')  # 载入按标准的cdecl调用协议导出的函数
-    # db_name = c_wchar_p(r'..\data\debug\score.ibd')
-    # db_type = DWORD(0x0A01)
-    # err = c_ulong()
-    # db_fh = HANDLE(dll.extr_db_fun_A(db_name, db_type, byref(err)))    # byref()函数用于获取对象的内存地址，db_fh是<class 'ctypes.c_void_p'>
+    def mount(self):
+        # 第一种：argtypes\regtype独立声明实参、返回值为wintypes类型，句柄类型int（绝对路径句柄较长）
+        # TODO: 路径不存在也返回句柄
+        # dll = windll.LoadLibrary(r'../x64/extr_db_krnl.dll')       # 载入按stdcall调用协议调用其中的函数
+        # db_name = r'C:\\Users\\XLY_LR\\PycharmProjects\\dll_test\\data\\debug\\score.ibd'
+        # db_type = 0x0A01
+        # err = DWORD(111)
+        self.dll.extr_db_fun_A.argtypes = [c_wchar_p, DWORD, POINTER(DWORD)]
+        self.dll.extr_db_fun_A.restype = HANDLE
+        self.db_fh = self.dll.extr_db_fun_A(self.db_name, self.db_type, byref(self.err))    # byref()函数用于获取对象的内存地址，db_fh是<class 'int'>，用pointer()也可
 
-    # dll = cdll.LoadLibrary(r'..\x64\extr_db_krnl.dll')  # 载入按标准的cdecl调用协议导出的函数
-    # db_name = r'..\data\debug\score.ibd'
-    # db_type = 0x0A01
-    # err = c_ulong()
-    # db_fh = HANDLE(dll.extr_db_fun_A(db_name, db_type, byref(err)))    # id()函数用于获取对象的内存地址，db_fh是<class 'ctypes.c_void_p'>
+        # 第二种：跟函数使用过程一起转换全部实参、返回值为wintypes的类型，句柄类型ctypes.c_void_p
+        # TODO: 路径不存在也返回句柄，无法写卸载
+        # dll = cdll.LoadLibrary(r'..\x64\extr_db_krnl.dll')  # 载入按标准的cdecl调用协议导出的函数
+        # db_name = c_wchar_p(r'..\data\debug\score.ibd')
+        # db_type = DWORD(0x0A01)
+        # err = c_ulong()
+        # db_fh = HANDLE(dll.extr_db_fun_A(db_name, db_type, byref(err)))    # byref()函数用于获取对象的内存地址，db_fh是<class 'ctypes.c_void_p'>
 
-    print(err)
-    print('HANDLE =', db_fh)
-    print(type(db_fh))
+        # 第三种：只声明实参err、返回值为wintypes类型，句柄类型ctypes.c_void_p
+        # TODO: 路径不存在也返回句柄，无法写卸载
+        # dll = cdll.LoadLibrary(r'..\x64\extr_db_krnl.dll')  # 载入按标准的cdecl调用协议导出的函数
+        # db_name = r'..\data\debug\score.ibd'
+        # db_type = 0x0A01
+        # err = c_ulong()
+        # db_fh = HANDLE(dll.extr_db_fun_A(db_name, db_type, byref(err)))
+
+        # 第四种：argtypes\regtype独立声明实参、返回值为wintypes类型，句柄类型int（相对路径句柄较短、有负值）
+        # TODO: 路径不存在也返回句柄，无法写卸载
+        # dll = cdll.LoadLibrary(r'../x64/extr_db_krnl.dll')       # 载入按cdecl调用协议调用其中的函数
+        # db_name = r'..\data\debug\score.ibd'
+        # db_type = 0x0A01
+        # err = c_ulong()
+        # db_fh = dll.extr_db_fun_A(db_name, db_type, byref(err))
+
+        print('ERROR = ', self.err)
+        print('HANDLE =', self.db_fh)
+        print(type(self.db_fh))
+        print("Mount Success!")
+        return self.err, self.db_fh
+
+    def unmount(self):
+        self.dll.extr_db_fun_B.argtypes = [POINTER(HANDLE)]
+        self.dll.extr_db_fun_B(byref(HANDLE(self.db_fh)))
+        print("Unmount Success!")
 
 
 def main():
-    mount_db_file()
+    m = MountAndUnmount()
+    err, db_fh = m.mount()
+    print(db_fh)
+    m.unmount()
+
 
 if __name__ == '__main__':
     main()
